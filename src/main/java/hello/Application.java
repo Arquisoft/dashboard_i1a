49df8cf5b0ace126b323f084af0a9aae05d3861b
unknown <UO244885@EII-L-13-10.aulas.uniovi.es>
2017-03-30 18:56:50 +0200

    Added dependencies to pom.xml, annotations and properties

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,8 +3,12 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("dashboard.model")
+@EnableJpaRepositories("dashboard.persistence")
 public class Application {
 
     public static void main(String[] args) {
