7bf3cf5e5b92666ddb740bba1807e9cf4ac8f1ef
MrSummat <gonzalomborge@hotmail.com>
2017-04-06 13:29:34 +0200

    Initial commit

--- src/test/java/ccmber/steps/ProposalSteps.java
+++ src/test/java/ccmber/steps/ProposalSteps.java
@@ -1,57 +1,100 @@
 package ccmber.steps;
 
+import static org.junit.Assert.assertFalse;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.net.URL;
 import java.util.List;
-import org.junit.After;
-import org.junit.Assert;
+
 import org.junit.Before;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.WebApplicationContext;
+
 import asw.hello.MainController;
+import cucumber.api.java.en.But;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+@SuppressWarnings({"deprecation","unused"})
 public class ProposalSteps {
 
     private static final Logger LOG = LoggerFactory.getLogger(MainController.class);
 
-//    private ConfigurableApplicationContext appContext;
-    private WebDriver driver = new HtmlUnitDriver();
-    private String baseUrl = "http://localhost:8080/";
-//    private Map<String, String> users = new HashMap<>();
+    @Value("${local.server.port}")
+    private int port;
+
+    private URL base;
+    private RestTemplate template;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
 
     @Before
-    public void setUp() {
-	driver.get(baseUrl);
-    }
-
-    @After
-    public void tearDown() throws Exception {
-	driver.quit();
+    public void setUp() throws Exception {
+	this.base = new URL("http://localhost:" + port);
+	template = new TestRestTemplate();
+	this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
     }
 
     @Given("^a list of proposals:$")
-    public void a_list_of_users(List<Prop> proposals) throws Throwable {
+    public void a_list_of_proposals(List<Prop> proposals) throws Throwable {
 	for (Prop p : proposals) {
 	    LOG.debug("Inserting proposal..." + p.name + " - " + p.id);
 	}
     }
 
-    @When("a proposal's linked is clicked$")
-    public void i_login_with_name_and_password(String name, String password) throws Throwable {
-	LOG.debug("Clicking on a proposal's link");
-	driver.findElement(By.xpath("//*[@id='link']")).click();
+    @When("I enter in a proposal$")
+    public void entring_proposal() throws Throwable {
+	LOG.debug("Entering a proposal");
     }
-
-    @Then("^I can see \"([^\"]*)\"$")
+    
+    @Given("^I am redirected to its proposal view page$")
+    public void redirection() throws Throwable {
+	LOG.debug("Entering a proposal");
+    }
+    
+    @Then("^I can see the \"([^\"]*)\"$")
     public void i_can_see(String text) throws Throwable {
 	LOG.debug("Checking text present: " + text);
-	List<WebElement> list = driver.findElements(By.xpath("//*[contains(text(),'" + text + "')]"));
-	Assert.assertTrue("Text not found!", list.size() > 0);
+	String body = "";
+	try {
+	    	text = body;
+		MvcResult result = this.mockMvc.perform(post("/viewProposal?id=1")
+				.accept(MediaType.TEXT_HTML_VALUE)).andExpect(status().isOk()).andReturn();
+		body = result.getResponse().getContentAsString();
+
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+    }
+    
+//    I cannot see other proposals
+    @But("^I cannot see other proposals$")
+    public void but_cannot_see() throws Throwable {
+	LOG.debug("Checking text not present");
+	String body = "";
+	try {
+		MvcResult result = this.mockMvc.perform(post("/admin")
+				.accept(MediaType.TEXT_HTML_VALUE)).andExpect(status().isOk()).andReturn();
+		body = result.getResponse().getContentAsString();
+
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	assertFalse(body.contains("Another proposal"));
     }
 
     public static class Prop {
--- src/test/resources/features/proposal.feature
+++ src/test/resources/features/proposal.feature
@@ -6,7 +6,7 @@ Scenario: Selecting a proposal on the list
       | Liberate snakes through the city    | 1  |
       | Another proposal					| 2	 |
       | Yet another proposal more 			| 3  |
- #  When I click on the hyperlink of the "Liberate snakes through the city" with id "1"
- #   Then I am redirected to its proposal view page
- #   And  I can see the "Content"
- #  But I cannot see other proposals
+   When I enter in a proposal
+   Then I am redirected to its proposal view page
+    And  I can see the "Content"
+   But I cannot see other proposals
